3d7daf415ebc7103463012ae6b3bb47b4379310f
Dave Hall <skwashd@gmail.com>
2018-12-02 21:40:26 +1100

    Redirect logout to the homepage

--- src/Controller/AuthenticationController.php
+++ src/Controller/AuthenticationController.php
@@ -100,18 +100,8 @@ class AuthenticationController extends ControllerBase {
    */
   public function logout() {
     user_logout();
-
-    $output = <<<EOF
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" >
-  <body>
-    <p>You have been logged out.</p>
-  </body>
-</html>
-EOF;
-
-    return new Response($output);
+    drupal_set_message(t('You have been logged out.'));
+    return $this->redirect('<front>');;
   }
 
   /**
